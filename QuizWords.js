d4efa21419d88f56dabb299c970cc9af6552b8ea
JGO204 <gonzalez204@usf.edu>
2023-04-24 11:47:45 -0400

    Final Project
    All Done.

---
+++ QuizWords.js
@@ -0,0 +1,65 @@
+let wordList = [
+    {
+        word: "trainapproachwarning",
+        hint: "TAW"
+    },
+
+    {
+        word: "ontrackSafety",
+        hint: "OTS"
+    },
+
+    {
+        word: "roadwayworkerincharge",
+        hint: "RWIC"
+    },
+
+    {
+        word:"personalprotectiveequipmenet",
+        hint: "PPE"
+    },
+
+    {
+        word: "roadwaymaintenancemachine",
+        hint: "RMM"
+    },
+
+    {
+        word: "individualtraindetection",
+        hint: "ITD"
+    },
+
+    {
+        word: "employeeincharge",
+        hint: "EIC"
+    },
+
+    {
+        word: "railroad",
+        hint: "RR"
+    },
+
+    {
+        word: "predeterminedplaceofsafety",
+        hint: "PPOS"
+    },
+
+    {
+        word: "federalrailroadadministration",
+        hint: "FRA"
+    },
+
+    {
+        word: "roadwayWorker",
+        hint: "RWW"
+    },
+
+    
+
+
+
+
+
+
+
+]
